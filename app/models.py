539d6054d465fd353e60b563b5c6e34e4b5b4dea
nightttt7 <nightttt7@gmail.com>
2020-03-15 17:37:06 +0100

    delete 'unique' title

--- app/models.py
+++ app/models.py
@@ -148,9 +148,9 @@ def load_user(user_id):
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.Text(length = 100), unique=True)
-    body = db.Column(db.Text(length = 100000))
-    body_html = db.Column(db.Text(length = 100000))
+    title = db.Column(db.Text)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
@@ -172,8 +172,8 @@ db.event.listen(Post.body, 'set', Post.on_changed_body)
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text(length = 100))
-    body = db.Column(db.Text(length = 100000))
+    name = db.Column(db.Text)
+    body = db.Column(db.Text)
     is_user = db.Column(db.Boolean, default=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
