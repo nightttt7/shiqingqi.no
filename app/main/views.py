ede8b57f147a091307d3a89c8f49e0b2e33c097b
nightttt7 <nightttt7@gmail.com>
2021-08-29 23:59:37 +0800

    add sametag and sameauthor page

--- app/main/views.py
+++ app/main/views.py
@@ -10,4 +10,24 @@ def index():
     pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=8, error_out=False)
     posts = pagination.items
-    return render_template('index.html', posts=posts, pagination=pagination)
+    return render_template('index_main.html', posts=posts, pagination=pagination)
+
+
+@main.route('/tag/<string:tag>', methods=['GET', 'POST'])
+def sametag(tag):
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.filter_by(tag=tag).order_by(Post.timestamp.desc()).paginate(
+        page, per_page=8, error_out=False)
+    posts = pagination.items
+    tag_ = Post.query.filter_by(tag=tag).first_or_404().tag
+    return render_template('index_sametag.html', posts=posts, pagination=pagination, tag_=tag_)
+
+
+@main.route('/author/<int:author_id>', methods=['GET', 'POST'])
+def sameauthor(author_id):
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.filter_by(author_id=author_id).order_by(Post.timestamp.desc()).paginate(
+        page, per_page=8, error_out=False)
+    posts = pagination.items
+    username = Post.query.filter_by(author_id=author_id).first_or_404().author.username
+    return render_template('index_sameauthor.html', posts=posts, pagination=pagination, username=username)
--- app/models.py
+++ app/models.py
@@ -146,6 +146,7 @@ class Post(db.Model):
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    tag = db.Column(db.Text)
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
     def __repr__(self):
