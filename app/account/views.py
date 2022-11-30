331c1088116db49545d089787bf52469bfa98704
nightttt7 <nightttt7@gmail.com>
2022-11-30 23:42:45 +0800

    add "Comments to your posts"

--- app/account/views.py
+++ app/account/views.py
@@ -1,7 +1,15 @@
 from flask import render_template
+from flask_login import current_user
 from . import account
+from .. import db
+from ..models import Permission, Role, User, Post, Comment
 
 
 @account.route('/')
 def index():
-    return render_template('account/index.html')
+    post_and_comment_s = []
+    for post in Post.query.filter_by(author_id=current_user.id):
+        for comment in post.comments:
+            post_and_comment_s.append({'post': post, 'comment': comment})
+    return render_template('account/index.html',
+                           post_and_comment_s=post_and_comment_s)
